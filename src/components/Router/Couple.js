fa35110503de6e55f4b66bcb4e47b4a57d076ee1
manju-cmd <manjusandy22995@gmail.com>
2021-08-04 19:14:19 +0530

    couples & individuals

--- src/components/Router/Couple.js
+++ src/components/Router/Couple.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import './bootstrap.min.css'
-// import './Couple.css' 
+import './bootstrap.min.css'
+import './Couple.css' 
 
 
 class Couple extends React.Component {
@@ -34,56 +34,59 @@ class Couple extends React.Component {
             </div>
             {/* End of Return-link */}
             {/* Start of main content */}
+            <div className="Arrow">
+                  <svg class="svg-inline--fa fa-chevron-left fa-w-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M243.658 43.114C227.69 27.834 202.362 28.365 187.096 44.364L11.098 228.356C-3.699 243.824 -3.699 268.198 11.098 283.666L187.096 467.657C194.955 475.875 205.471 480 216.002 480C225.94 480 235.908 476.313 243.658 468.907C259.611 453.626 260.174 428.315 244.908 412.347L95.363 256.011L244.908 99.674C260.174 83.706 259.611 58.395 243.658 43.114Z"></path></svg>
+                  </div>
             <div className="container main-content">
               <div className="row">
                 <div className="col-lg-4 col-md-2 col-sm-1 d-sm-block d-none" />
                 <div className="col-lg-4 col-md-8 col-sm-10 individual-content">
                   <div className="hero-text-container">
-                    <h1 className="hero-title">Welcome</h1>
-                    <p className="hero-text">What are some areas that you and<br></br> your partner want to focus on, or<br></br> get help with?</p>
+                    <h1 className="hero-title" style={{marginLeft:"5px",lineHeight:2,marginTop:"-30px",fontWeight:400}}>Welcome</h1>
+                    <p className="hero-text" style={{marginLeft:"4px"}}>What are some areas that you and<br></br> your partner want to focus on, or<br></br> get help with?</p>
                   </div>
                   <div className="checkbox-container">
-                    <ul className="checkbox-lists">
+                    <ul className="checkbox-lists" style={{marginLeft:"-1px", fontSize:18}}> 
                       {/* link 1 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Communication</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}} >Communication</p>
                       </li>
                       {/* link 2 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Money</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}}>Money</p>
                       </li>
                       {/* link 3 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Children</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}}>Children</p>
                       </li>
                       {/* link 4 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Work</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}}>Work</p>
                       </li>
                       {/* link 5 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Intimacy</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}}>Intimacy</p>
                       </li>
                       {/* link 6 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Something Else</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}}>Something Else</p>
                       </li>
                       {/* End of links*/}
                     </ul>
-                    <div className="skip">
-                    <p>Skip and <span><a href="./schedule.html">schedule</a></span> your free online consultation.</p></div>
+                    <div className="skip" style={{marginLeft:"30px"}} >
+                    <p style={{color:"#fff",fontweight:400,fontSize:20}}>Skip and schedule your free online consultation.</p></div>
                   </div>
                 </div>
                 <div className="col-lg-4 col-md-2 col-sm-1 d-sm-block d-none" />
@@ -93,7 +96,7 @@ class Couple extends React.Component {
                 <div className="col-lg-4 col-md-3 col-sm-2 col-2" />
                 <div className="col-lg-4 col-md-6 col-8">
                   <div className="linear-btn-container">
-                    <button type="button" className="linear-btn" onclick="window.location = './about-app.html'">Continue</button>
+                    <button  style={{marginLeft:"5px",backgroundColor:"#d7e0e9a6"}} type="button" className="linear-btn" onclick="window.location = './about-app.html'">Continue</button>
                   </div>
                 </div>
                 <div className="col-lg-4 col-md-3 col-2" />
--- src/components/Router/Individual.js
+++ src/components/Router/Individual.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import './Indivi.css';
 import './bootstrap.min.css'
+import './Aboutapp.css'
 
 class Individual extends React.Component {
     render(){
@@ -31,57 +32,61 @@ class Individual extends React.Component {
             </div>
             {/* End of Return-link */}
             {/* Start of main content */}
+            <div className="Arrow">
+                  <svg class="svg-inline--fa fa-chevron-left fa-w-10" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M243.658 43.114C227.69 27.834 202.362 28.365 187.096 44.364L11.098 228.356C-3.699 243.824 -3.699 268.198 11.098 283.666L187.096 467.657C194.955 475.875 205.471 480 216.002 480C225.94 480 235.908 476.313 243.658 468.907C259.611 453.626 260.174 428.315 244.908 412.347L95.363 256.011L244.908 99.674C260.174 83.706 259.611 58.395 243.658 43.114Z"></path></svg>
+                  </div>
             <div className="container main-content">
               <div className="row">
                 <div className="col-lg-4 col-md-2 col-sm-1 d-sm-block d-none" />
                 <div className="col-lg-4 col-md-8 col-sm-10 individual-content">
                   <div className="hero-text-container">
-                    <h1 className="hero-title" style={{marginLeft:"1px",lineHeight:2,marginTop:"-30px"}}>Welcome</h1>
-                    <p className="hero-text" style={{marginLeft:"2px"}}>What are some areas that you want <br></br>to focus on, or get help with?</p>
+                    <h1 className="hero-title" style={{marginLeft:"5px",lineHeight:2,marginTop:"-30px",fontWeight:400}}>Welcome</h1>
+                    <p className="hero-text" style={{marginLeft:"4px"}}>What are some areas that you want <br></br>to focus on, or get help with?</p>
                   </div>
+                 
                   <div className="checkbox-container">
-                    <ul className="checkbox-lists" style={{marginLeft:"50px"}}>
+                    <ul className="checkbox-lists" style={{marginLeft:"-1px", fontSize:18}}>
                       {/* Link 1 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Anxiety</p>
+                        <p style={{color:"#fff", fontweight:400,fontSize:20}}>Anxiety</p>
                       </li>
                       {/* Link 2 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Depression</p>
+                        <p style={{color:"#fff",fontweight:400,fontSize:20}}>Depression</p>
                       </li>
                       {/* Link 3 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Sleep</p>
+                        <p style={{color:"#fff",fontweight:400,fontSize:20}}>Sleep</p>
                       </li>
                       {/* Link 4 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Professional Development</p>
+                        <p style={{color:"#fff",fontweight:400,fontSize:20}}>Professional Development</p>
                       </li>
                       {/* Link 5 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Exercise or Diet</p>
+                        <p style={{color:"#fff",fontweight:400,fontSize:20}}>Exercise or Diet</p>
                       </li>
                       {/* Link 6 */}
                       <li className="checkbox-links">
                         <input type="checkbox" className="check-box" />
                         <span className="custom-checkbox" />
-                        <p>Something Else</p>
+                        <p style={{color:"#fff",fontweight:400,fontSize:20}}>Something Else</p>
                       </li>
                       {/* End of links */}
                     </ul>
                     </div>
-                    <div className="skip">
-                    <p>Skip and <span><a href="./schedule.html">schedule</a></span> your free online consultation.</p>
+                    <div className="skip" style={{marginLeft:"30px"}}>
+                    <p style={{color:"#fff",fontweight:400,fontSize:20}}>Skip and schedule your free online consultation.</p>
                   </div>
                 </div>
                 <div className="col-lg-4 col-md-2 col-sm-1 d-sm-block d-none" />
@@ -91,7 +96,7 @@ class Individual extends React.Component {
                 <div className="col-lg-4 col-md-3 col-sm-2 col-2" />
                 <div className="col-lg-4 col-md-6 col-8">
                   <div className="linear-btn-container">
-                    <button type="button" className="linear-btn" onclick="window.location = './about-app.html'">Continue</button>
+                    <button style={{marginLeft:"5px",backgroundColor:"#d7e0e9a6"}} type="button" className="linear-btn" onclick="window.location = './about-app.html'">Continue</button>
                   </div>
                 </div>
                 <div className="col-lg-4 col-md-3 col-2" />
